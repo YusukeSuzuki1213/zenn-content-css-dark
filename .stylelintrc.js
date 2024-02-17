dc277136ea88c6a9ac3f45fa5698b9dce7485b5d
Yusuke <yusuke.suzuki.2024@gmail.com>
2024-02-17 10:50:57 +0900

    added: lint

---
+++ .stylelintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+      'stylelint-config-recommended-scss'
+    ],
+    rules: {
+      'no-invalid-position-at-import-rule': null,
+      'no-descending-specificity': null,      
+    }
+ };
+ 
